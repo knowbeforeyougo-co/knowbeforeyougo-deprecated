10acb94f27768bcb4581f71933626b964b5dd0ba
yakovliam <jacob@yakovliam.com>
2021-06-15 21:04:08 -0400

    :tada: Initialize client

---
+++ client/src/test/java/com/yakovliam/knowbeforeyougo/client/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yakovliam.knowbeforeyougo.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
